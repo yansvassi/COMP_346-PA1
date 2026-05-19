92b3a69ffbe613ab1fd77eefd672e321b43bec12
yansvassi <yana.vassilyev@outlook.com>
2026-05-19 13:16:41 -0400

    Initial commit

---
+++ Driver.java
@@ -0,0 +1,26 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kerly Titus
+ */
+public class Driver {
+
+    /** 
+     * main class
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    	
+    	 /*******************************************************************************************************************************************
+    	  * TODO : implement all the operations of main class - activate network and start client (sending & receiving) and server 																					*
+    	  ******************************************************************************************************************************************/
+        
+    	
+    }
+}
